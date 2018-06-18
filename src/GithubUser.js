f2c80c4ceaaa8428cc72ec92dcd5728a71d54a1e
Zachary Spitzer <zspitzer@nd.edu>
2018-06-18 15:24:27 -0400

    Fetch user info using Github API

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react'
 
 class GithubUser extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            user: {}
+        }
+        
+        this.fetchUserData()
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.match.params.username !== this.props.match.params.username) {
+            this.fetchUserData()
+        }
+    }
+
+    fetchUserData = () => {
+        const { params } = this.props.match
+        fetch(`https://api.github.com/users/${params.username}`)
+            .then(response => response.json())
+            .then(user => this.setState({ user }))
+    }
+
+
   render() {
-    const { params } = this.props.match
+    const { user } = this.state
 
     return (
       <div className="GithubUser">
-        <h1>GitHub user: {params.username}</h1>
+        <img src={user.avatar_url} alt="" />
+        <h2>
+          <a href={user.html_url} target="_blank">
+            {user.name} ({user.login})
+          </a>
+        </h2>
+        <h3>{user.location}</h3>
       </div>
     )
   }
